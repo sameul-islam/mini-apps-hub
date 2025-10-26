e15867f76db3d9d0dd8e3b56b721883bb6bdb258
Sameul Islam <sameulislam479@gmail.com>
2025-10-26 17:09:43 +0600

    Update FileUploader.tsx

--- src/app/apps/file-converter/components/FileUploader.tsx
+++ src/app/apps/file-converter/components/FileUploader.tsx
@@ -1,41 +1,41 @@
-"use client";
+// "use client";
 
-import React, { ChangeEvent, RefObject } from "react";
+// import React, { ChangeEvent, RefObject } from "react";
 
-type Props = {
-  onFiles: (files: FileList | File[]) => void;
-  inputRef?: React.RefObject<HTMLInputElement>;
-};
+// type Props = {
+//   onFiles: (files: FileList | File[]) => void;
+//   inputRef?: React.RefObject<HTMLInputElement>;
+// };
 
-export default function FileUploader({ onFiles, inputRef }: Props) {
-  const handleFiles = (e: ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files) onFiles(e.target.files);
-  };
+// export default function FileUploader({ onFiles, inputRef }: Props) {
+//   const handleFiles = (e: ChangeEvent<HTMLInputElement>) => {
+//     if (e.target.files) onFiles(e.target.files);
+//   };
 
-  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-  };
+//   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+//     e.preventDefault();
+//   };
 
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    if (e.dataTransfer.files) onFiles(e.dataTransfer.files);
-  };
+//   const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+//     e.preventDefault();
+//     if (e.dataTransfer.files) onFiles(e.dataTransfer.files);
+//   };
 
-  return (
-    <div
-      className="border-dashed border-2 border-gray-400 p-6 rounded cursor-pointer hover:border-gray-600 transition-colors"
-      onDragOver={handleDragOver}
-      onDrop={handleDrop}
-      onClick={() => inputRef?.current?.click()}
-    >
-      <input
-        type="file"
-        multiple
-        onChange={(e) => e.target.files && onFiles(e.target.files)}
-        ref={inputRef}
-        className="hidden"
-      />
-      <p>Drag & drop files here or click to select</p>
-    </div>
-  );
-}
+//   return (
+//     <div
+//       className="border-dashed border-2 border-gray-400 p-6 rounded cursor-pointer hover:border-gray-600 transition-colors"
+//       onDragOver={handleDragOver}
+//       onDrop={handleDrop}
+//       onClick={() => inputRef?.current?.click()}
+//     >
+//       <input
+//         type="file"
+//         multiple
+//         onChange={(e) => e.target.files && onFiles(e.target.files)}
+//         ref={inputRef}
+//         className="hidden"
+//       />
+//       <p>Drag & drop files here or click to select</p>
+//     </div>
+//   );
+// }
